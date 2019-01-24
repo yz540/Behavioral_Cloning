49daac21605250d2ca4438d3147f52e6034f7525
yz540 <yanyanoo101@hotmail.com>
2019-01-24 14:04:19 +0000

    modify the nn model and add fine tune

--- model.py
+++ model.py
@@ -4,12 +4,14 @@ import numpy as np
 from scipy import ndimage
 # Setup Keras
 from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Dropout
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers import Cropping2D
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
+from keras.models import load_model
+from keras import optimizers
 
 def processImg(img_path):
     image = ndimage.imread(img_path)
@@ -56,7 +58,7 @@ def load_data(path):
     samples = []
     with open(path + 'driving_log.csv') as csvfile:
         reader = csv.reader(csvfile)
-#         next(reader, None) # skip header of sample data
+        next(reader, None) # skip header of sample data
         for line in reader:
             samples.append(line)
     return samples
@@ -65,25 +67,44 @@ def load_data(path):
 def nn_model(train_generator, validation_generator):
     # compile and train the model using the generator function
     model = Sequential()
-    
     model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(70,320,3)))
     model.add(Conv2D(6, 5, 5, activation='relu'))
     model.add(MaxPooling2D())
     model.add(Conv2D(6, 5, 5, activation='relu'))
     model.add(MaxPooling2D())
     model.add(Flatten())
-    model.add(Dense(120))
+    model.add(Dropout(.5))
     model.add(Dense(84))
+    model.add(Dropout(.5))
     model.add(Dense(1))
+    print(model.summary())
     model.compile(loss='mse', optimizer='adam')
 #     model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
-    model.fit_generator(train_generator, samples_per_epoch= len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
+#     model.fit_generator(train_generator, samples_per_epoch= len(train_samples), validation_data=validation_generator,   nb_val_samples=len(validation_samples), nb_epoch=3)
+    model.fit_generator(train_generator, steps_per_epoch= len(train_samples), validation_data=validation_generator, validation_steps=len(validation_samples), epochs=3, verbose = 1)
     return model
 
-sample_data_path = "../../../opt/carnd_p3/train_data/"
+def transfer_learning(model_path, train_generator, validation_generator):
+    ft_model = load_model(model_path)
+    ft_model.load_weights(model_path)
+    ft_model.compile(loss='mse', optimizer='adam')
+    ft_model.fit_generator(train_generator, samples_per_epoch = len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=1)
+    return ft_model
+
+# first round trian
+sample_data_path = "../../../opt/carnd_p3/data/"
+# # fine tune data
+# sample_data_path = "../../../opt/carnd_p3/train_data/"
 samples = load_data(sample_data_path)
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 train_generator = generator(sample_data_path, train_samples, batch_size=32)
 validation_generator = generator(sample_data_path, validation_samples, batch_size=32)
+
+# Train the model from scratch
 model = nn_model(train_generator, validation_generator)
 model.save('model_train.h5')  # creates a HDF5 file 'my_model.h5'
+
+# # Use transfer learning with a pretrained model
+# model_path = 'model_train.h5'
+# ft_model = transfer_learning(model_path, train_generator, validation_generator)
+# ft_model.save('ft_model.h5')
